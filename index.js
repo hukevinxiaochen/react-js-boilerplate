5b4ac774421ce40c12307cf2c7199ac0829bb1dd
hukevinxiaochen <hukevinxiaochen@gmail.com>
2016-05-15 09:35:01 -0400

    Set up Material-UI dependencies: react-tap-event-plugin and Roboto Font

--- index.js
+++ index.js
@@ -8,6 +8,8 @@
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+var injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
 
 // React Components
 
