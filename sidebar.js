8789bb3a66f9deb34c42ced028e1db196cdc6c66
hukevinxiaochen <hukevinxiaochen@gmail.com>
2016-05-15 21:57:18 -0400

    Implement Drawer component from Material-UI.

---
+++ sidebar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Drawer from 'material-ui/Drawer';
+import MenuItem from 'material-ui/MenuItem';
+
+const SideBar = () => (
+    <Drawer>
+    <MenuItem>Welcome</MenuItem>
+    <MenuItem>About Us</MenuItem>
+    </Drawer>
+);
+export default SideBar;
