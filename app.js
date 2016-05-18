8b52ae392bca2e54cb4df1b605c76482f4280d8a
hukevinxiaochen <hukevinxiaochen@gmail.com>
2016-05-18 19:00:00 -0400

    Make responsive AppBar and Drawer.
    AppBar show/hide behavior is controlled by react-responsive library.
    
    Drawer show/hide and toggle is controlled by custom-rolled event
    listening to matchMedia changes.

--- app.js
+++ app.js
@@ -5,14 +5,25 @@ var React = require('react');
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Paper from 'material-ui/Paper';
+import AppBar from 'material-ui/AppBar';
 import SideBar from './sidebar';
 
+// node_modules/react-responsive
+import MediaQuery from 'react-responsive';
+
 var App = React.createClass({
+    toggleSideBar: function () { 
+        this.drawer.handleToggle();
+    },
     render: function () {
         return(
             <MuiThemeProvider muiTheme={getMuiTheme()}>
                 <div>
-                    <SideBar className="sidebar" />
+                    <MediaQuery maxWidth={840}>
+                        <AppBar title="E &amp; K" 
+                            onLeftIconButtonTouchTap={this.toggleSideBar} />
+                    </MediaQuery>
+                    <SideBar ref={(ref) => this.drawer = ref} className="sidebar" />
                     <section>
                         {this.props.children}
                     </section>
--- sidebar.js
+++ sidebar.js
@@ -5,9 +5,36 @@ import Link from 'react-router/lib/Link';
 import IndexLink from 'react-router/lib/IndexLink'
 
 var SideBar = React.createClass({
+    getDefaultProps: function () {
+        return {};
+    },
+    getInitialState: function () { 
+        return {
+            open: !window.matchMedia('(max-width: 840px)').matches 
+        }
+    },
+    handleToggle: function () {
+        this.setState({
+            open: !this.state.open 
+        });
+    },
+    handleClose: function (mql) {
+        if (mql.matches) {
+            this.setState({open: false});
+        }
+        else {
+            this.setState({open: true});
+        }
+    },
+    componentDidMount: function () {
+        window.matchMedia('(max-width: 840px)').addListener(this.handleClose);
+    },
+    componentWillUnmount: function () {
+        window.matchMedia('(max-width: 840px)').removeListener(this.handleClose);
+    },
     render: function () {
         return (
-            <Drawer width={200}>
+            <Drawer width={200} open={this.state.open}>
                 <IndexLink to="/"><MenuItem>Home</MenuItem></IndexLink>
                 <Link to="/about-us"><MenuItem>About Us</MenuItem></Link>
                 <MenuItem>Wedding Party</MenuItem>
