3e493253416fc3f9bec843d7c949a253cd39720e
hukevinxiaochen <hukevinxiaochen@gmail.com>
2016-05-20 11:38:44 -0400

    Comment better. Import page components.

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
  * Front-end libraries we rely on.
  * - React
  * - Material-UI
+ * - react-router
+ * - react-responsive
 */
 
 var React = require('react');
@@ -11,30 +13,42 @@ var ReactDOM = require('react-dom');
 var injectTapEventPlugin = require("react-tap-event-plugin");
 injectTapEventPlugin();
 
-// Import Material-UI stuff
-import App from './app';
-import Home from './home';
-import AboutUs from './aboutUs';
-import Registry from './registry';
 
-// React Router
+// App Components
+import App from './components/app';
+
+// Pages
+import Home from './pages/home';
+import AboutUs from './pages/aboutUs';
+import WeddingParty from './pages/weddingparty';
+import CeremonyAndReception from './pages/ceremonyreception';
+import Accommodations from './pages/accommodations';
+import Transportation from './pages/transportation';
+import ThingsToDo from './pages/thingstodo';
+import Registry from './pages/registry';
+
+// Routing
 import Router from 'react-router/lib/Router';
 import Route from 'react-router/lib/Route';
 import hashHistory from 'react-router/lib/hashHistory';
 import IndexRoute from 'react-router/lib/IndexRoute';
 
+var pageD = {
+    aboutus: "/about-us" 
+};
+
 // Render
 ReactDOM.render((
     <Router history={hashHistory}>
         <Route path="/" component={App}>
-            <IndexRoute component={Home}/>
-            <Route path="/about-us" component={AboutUs}/>
-            <Route path="/registry" component={Registry}/>
+            <IndexRoute component={Home} />
+            <Route path={pageD.aboutus}  component={AboutUs} />
+            <Route path="/ceremony-reception" component={CeremonyAndReception} />
+            <Route path="/wedding-party" component={WeddingParty} />
+            <Route path="/accommodations" component={Accommodations} />
+            <Route path="/transportation" component={Transportation} />
+            <Route path="/things-to-do" component={ThingsToDo} /> 
+            <Route path="/registry" component={Registry} />
         </Route>
     </Router>
 ), document.getElementById('app'));
-
-// ReactDOM.render(
-//     <HelloWorld />,
-//     document.getElementById('greeting')
-// );
